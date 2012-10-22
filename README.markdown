feb9310debee0c935fdd1000483e99f470380ef2
Brian Muller <brian.muller@livingsocial.com>
2012-10-22 11:27:56 -0400

    updated project w/ better css

--- README.markdown
+++ README.markdown
@@ -14,15 +14,19 @@ First, load jQuery, timeago, and showoff:
 <script src="showoff.min.js" type="text/javascript"></script>
 ```
 
-Then, specify the div you want to fill and the repos per username you want to show off.  For instance, let's say I wanted to show off bmuller's sexmachine and bandit projects in the div named "repos":
+Then, specify the div you want to fill and the repos per username you want to show off.  For instance, let's say I wanted to show off bmuller's sexmachine and bandit projects and LivingSocial's ankusa in the div named "repos":
 
 ```html
 <script type="text/javascript">
   $(function() {
-    SHOWOFF.load('repos', { 'bmuller': [ 'bandit', 'sexmachine' ] });
+    SHOWOFF.load('repos', { 'bmuller': [ 'bandit', 'sexmachine' ], 'livingsocial': [ 'ankusa' ] });
   });
 </script>
 ```
+
+This results in:
+
+![Example image](example.png)
 
 ## License
 
--- showoff.js
+++ showoff.js
@@ -1,24 +1,29 @@
 var SHOWOFF = {
    repos: null,
    div: null,
+   inited: false,
+   init: function() {
+      $.ajaxSetup({ cache: true });
+      SHOWOFF.setStyles();
+      SHOWOFF.inited = true;
+   },
    load: function(div, repos) {
       SHOWOFF.div = "#" + div;
       SHOWOFF.repos = repos;
-      $.ajaxSetup({ cache: true });
-      if($('#showoff-styles').length == 0) SHOWOFF.setStyles();
+      if(!SHOWOFF.inited) SHOWOFF.init();
       $(div).hide();
       for(username in repos)
 	 $.getScript("https://api.github.com/users/" + username + "/repos?callback=SHOWOFF.showRepos")
    },
    setStyles: function() {
       var style = ".repo { font-family: Helvetica,arial,freesans,clean,sans-serif; border: 1px solid; " +
-	 "border-color: #eee #ccc #ccc #eee; padding: 15px; margin: 10px; float: left; width: 400px; }" +
+	 "display: inline-block; border-color: #ccc #aaa #aaa #ccc; padding: 10px; margin: 5px; width: 350px; }" +
 	 ".repo a { text-decoration: none; color: #4183C4; }" +
 	 ".repo a.pname { font-size: 20px; font-weight: bold; }" +
 	 ".repo a:hover { text-decoration: underline; }" +
 	 ".repo div.lang { color: #666; font-size: 11px; font-weight: bold; }" +
 	 ".repo div.lastupdate { font-size: 11px; color: #888; }" +
-	 ".repo div.description { margin: 5px 0px 5px 0px; }" +
+	 ".repo div.description { margin: 5px 0px 5px 0px; height: 36px; }" +
 	 ".repo div.pull-right { float: right; }" +
 	 ".repo img { float: left; width: 20px; height: 20px; margin-right: 5px; }";
       $("head").append("<style id=\"showoff-styles\">" + style + "</style>");
@@ -32,6 +37,9 @@ var SHOWOFF = {
       $("span.timeago").timeago();
       $(SHOWOFF.div).fadeIn();
    },
+   truncate: function(text, size) {
+      return (text.length > size) ? text.substring(0, size-3) + "..." : text;
+   },
    buildHTML: function(repo) {
       var stars = repo.watchers + " Star" + (repo.watchers == 1 ? '' : 's'), forks = repo.forks + " Fork" + (repo.forks == 1 ? '' : 's');
       return "<div class=\"repo\"><a href=\"https://github.com/" + repo.owner.login + "\" class=\"avatar\">" + 
@@ -39,7 +47,7 @@ var SHOWOFF = {
 	 repo.name + "</a><div class=\"pull-right lang\">" + repo.language +" &#9679; " +
 	 "<a href=\"" + repo.html_url + "/stargazers\">" + stars  + "</a> &#9679; " +
 	 "<a href=\"" + repo.html_url + "/network\">" + forks + "</a></div></div>" +
-	 "<div class=\"description\">" + repo.description + "</div><div class=\"lastupdate\">Last updated " +
+	 "<div class=\"description\">" + SHOWOFF.truncate(repo.description, 97) + "</div><div class=\"lastupdate\">Last updated " +
 	 "<span class=\"timeago\" title=\"" + repo.pushed_at + "\">" + repo.pushed_at + "</span></div></div>";
    }
 };
